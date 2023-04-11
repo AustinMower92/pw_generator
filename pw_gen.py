8964c78064bdbbab73f4a11f8b1805749e1876a6
AustinMower92 <austin.mower9@gmail.com>
2023-04-11 18:53:00 -0400

    required range to be between 8 and 25 / cleaned up code some

--- pw_gen.py
+++ pw_gen.py
@@ -4,13 +4,7 @@ import pyperclip
 
 # code imported from local sandbox
 
-def create_pw(pw_length):
-    digits = string.digits
-    special_chars = string.punctuation
-    letters = string.ascii_letters
-
-    # collection of alphabet (lower & uppercase) | digits 0-9 | standard special chars
-    alphabet = letters + digits + special_chars
+def generate_pwd(pw_length, alphabet, special_chars, digits):
     pwd_strong = False
     while not pwd_strong:
         pwd = ''
@@ -23,25 +17,35 @@ def create_pw(pw_length):
 
         if (has_special_chars and has_min_nums):
             # meets reqs -> turn pwd string flag to True
-            pwd_strong = True         
+            pwd_strong = True                   
+    return pwd
+
+def create_pw(pw_length):
+    digits = string.digits
+    special_chars = string.punctuation
+    letters = string.ascii_letters
+
+    # collection of alphabet (lower & uppercase) | digits 0-9 | standard special chars
+    alphabet = letters + digits + special_chars
+    pwd = generate_pwd(pw_length, alphabet, special_chars, digits)
     return pwd
 
 def run_pw_gen():
    print('Welcome to Password Generator!')
    # data type validation | program can only accept integers
    try:
-      pw_length = int(input('Please enter your desired password length (1-25 / integers only), then press enter: '))
+      pwd_length = int(input('Please enter your desired password length (8-25 / integers only), then press enter: '))
    except Exception:
-      print('Invalid entry. Please enter an integer between 1 and 25.')
+      print('Invalid entry. Please enter an integer between 8 and 25.')
    else:
       # more validation | pwd_length must be 1-25
-      if pw_length < 26 and pw_length > 0:
-         pwd = create_pw(pw_length)
+      if pwd_length < 26 and pwd_length > 7:
+         pwd = create_pw(pwd_length)
          print(f'Your New Password is: {pwd}\nPassword Length: {len(pwd)}')
          # copy password to user clipboard
          pyperclip.copy(pwd)
          print('New password copied to clipboard!')
       else:
-         print('Invalid entry. Please enter an integer between 1 and 25.')
+         print('Invalid entry. Please enter an integer between 8 and 25.')
 
 run_pw_gen()
